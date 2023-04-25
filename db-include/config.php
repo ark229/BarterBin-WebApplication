5b2756ec46750a708bb27f1fd109944c4e30dd80
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-25 12:23:44 -0400

    update config file

--- db-include/config.php
+++ db-include/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$username = 'your_username';
-$password = 'your_password';
-$dbname = 'your_database_name';
+$host = 'us-cdbr-east-06.cleardb.net';
+$username = 'b3588185a24fe4';
+$password = 'ebdf00d5';
+$dbname = 'heroku_24d833d07b8f60f';
 
 $connection = new mysqli($host, $username, $password, $dbname);
 
