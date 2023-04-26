e6ef4d6a0fc5f929443d5cd36b0570e243a53f42
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-26 12:09:30 -0400

    update

--- config.php
+++ config.php
@@ -5,6 +5,11 @@ $password = 'ebdf00d5';
 $dbname = 'heroku_24d833d07b8f60f';
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $connection = new mysqli($servername, $username, $password, $dbname);
 
 // Create connection
