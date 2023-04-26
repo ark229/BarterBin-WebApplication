b94597ecbc1a4b7194f06795b25ed0dd34467cdf
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-26 15:19:46 -0400

    update code for items table

--- submit.php
+++ submit.php
@@ -1,15 +1,11 @@
-/*
 <?php
 require_once('config.php');
 
-//$password_hash = password_hash($_POST["passwd"], PASSWORD_DEFAULT);
 $sql = "INSERT INTO users (first_name, last_name, email, city, state_name, passwd, date_added)
         VALUES (?, ?, ?, ?, ?, ?, NOW())";
 
-
 $stmt = mysqli_stmt_init($conn);
 mysqli_stmt_prepare($stmt, $sql);
-
 
 mysqli_stmt_bind_param(
     $stmt,
@@ -22,11 +18,26 @@ mysqli_stmt_bind_param(
     $_POST["passwd"]
 );
 
-
 mysqli_stmt_execute($stmt);
+
+// Get the user_id of the newly inserted user
+$user_id = mysqli_insert_id($conn);
+
+// Insert needs and offers data into the "items" table
+$sql2 = "INSERT INTO items (user_id, needs, offers, date_added)
+         VALUES (?, ?, ?, NOW())";
+
+$stmt2 = mysqli_stmt_init($conn);
+mysqli_stmt_prepare($stmt2, $sql2);
+
+mysqli_stmt_bind_param(
+    $stmt2,
+    "iss",
+    $user_id,
+    $_POST["needs"],
+    $_POST["offers"]
+);
+
+mysqli_stmt_execute($stmt2);
+
 header("Location: success.php");
-
-
-?>
-
-*
