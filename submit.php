45a7f46a3d83f2a2f338294e36e650eef757d4a5
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-26 13:54:11 -0400

    clean up code

--- submit.php
+++ submit.php
@@ -26,56 +26,6 @@ mysqli_stmt_bind_param(
 mysqli_stmt_execute($stmt);
 header("Location: success.php");
 
-/*
-
-require_once('session.php');
-
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$city = $_POST['city'];
-$state_name = $_POST['state_name'];
-$passwd = $_POST['passwd'];
-
-// Hash the password for security
-$hashed_password = password_hash($passwd, PASSWORD_DEFAULT);
-
-$sql = "INSERT INTO users (first_name, last_name, email, city, state_name, passwd, date_added) VALUES (?, ?, ?, ?, ?, ?, NOW())";
-
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ssssss", $first_name, $last_name, $email, $city, $state_name, $hashed_password);
-//header("Location: /login.php"); // Redirect to the login page after successful registration
-
-if ($stmt->execute()) {
-    header("Location: /login.php"); // Redirect to the login page after successful registration
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$stmt->close();
-$conn->close();
-
-*/
-
-//if (!$stmt->prepare($sql)) {
-//die("SQL error: " . $mysqli->error);
-//}
-
-
-/*
-if ($stmt->execute()) {
-
-    header("Location: success.php");
-    exit;
-} else {
-
-    if ($mysqli->errno === 1062) {
-        die("email already taken");
-    } else {
-        die($mysqli->error . " " . $mysqli->errno);
-    }
-}
-
-*/
 
 ?>
+
