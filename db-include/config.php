9ef8fea58fef0d3e82f5a6ae826ab69f21d56823
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-25 22:42:16 -0400

    update

--- db-include/config.php
+++ db-include/config.php
@@ -7,6 +7,16 @@ $dbname = 'heroku_24d833d07b8f60f';
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $connection = new mysqli($host, $username, $password, $dbname);
 
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+
+// Check the connection
+if ($conn->connect_error) {
+    // Define your custom error message here
+    $errorMessage = "We're sorry, but we're unable to connect to the database at the moment. Please try again later.";
+
+    // You can log the error message for debugging purposes
+    error_log("Connection failed: " . $conn->connect_error);
+
+    // Display the custom error message to the user
+    echo $errorMessage;
+    exit;
 }
