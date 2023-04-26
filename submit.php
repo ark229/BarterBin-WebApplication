306e48904d1373bb4c27d507da1b69a7fb439877
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-26 15:07:29 -0400

    update test

--- submit.php
+++ submit.php
@@ -6,12 +6,12 @@ require_once('config.php');
 //$password_hash = password_hash($_POST["passwd"], PASSWORD_DEFAULT);
 $sql = "INSERT INTO users (first_name, last_name, email, city, state_name, passwd, date_added)
         VALUES (?, ?, ?, ?, ?, ?, NOW())";
-$sql2 = "INSERT INTO items (needs, offers, date_added) VALUES (?, ?, NOW())";
+//$sql2 = "INSERT INTO items (needs, offers, date_added) VALUES (?, ?, NOW())";
 
 //$stmt = $conn->stmt_init();
 $stmt = mysqli_stmt_init($conn);
 mysqli_stmt_prepare($stmt, $sql);
-mysqli_stmt_prepare($stmt, $sql2);
+//mysqli_stmt_prepare($stmt, $sql2);
 
 mysqli_stmt_bind_param(
     $stmt,
@@ -24,12 +24,14 @@ mysqli_stmt_bind_param(
     $_POST["passwd"]
 );
 
+/*
 mysqli_stmt_bind_param(
     $stmt,
     "ss",
     $_POST["needs"],
     $_POST["offers"],
 );
+*/
 
 mysqli_stmt_execute($stmt);
 header("Location: success.php");
