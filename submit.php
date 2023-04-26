3f5d3da176103c57fdc8ca63ccdef7ef3aac89f6
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-26 14:43:07 -0400

    update submit

--- submit.php
+++ submit.php
@@ -4,9 +4,10 @@
 require_once('config.php');
 
 //$password_hash = password_hash($_POST["passwd"], PASSWORD_DEFAULT);
-
 $sql = "INSERT INTO users (first_name, last_name, email, city, state_name, passwd, date_added)
-        VALUES (?, ?, ?, ?, ?, ?, NOW())";
+        VALUES (?, ?, ?, ?, ?, ?, NOW());
+
+        INSERT INTO items (needs, offers, date_added) VALUES (?, ?, NOW())";
 
 //$stmt = $conn->stmt_init();
 $stmt = mysqli_stmt_init($conn);
@@ -21,6 +22,13 @@ mysqli_stmt_bind_param(
     $_POST["city"],
     $_POST["state_name"],
     $_POST["passwd"]
+);
+
+mysqli_stmt_bind_param(
+    $stmt,
+    "ss",
+    $_POST["needs"],
+    $_POST["offers"],
 );
 
 mysqli_stmt_execute($stmt);
