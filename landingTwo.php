fa3700a9cb6e85d4bb94e53792515616d3ad9d7c
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-25 20:09:11 -0400

    update

--- landingTwo.php
+++ landingTwo.php
@@ -8,11 +8,39 @@ require_once('db-include/session.php');
 if (!isset($_SESSION['needs']))
   header("location: /index.php");
 
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$city = $_POST['city'];
+$state = $_POST['state_name'];
 $password = $_POST['passwd'];
+
+// Hash the password for security
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$query = "INSERT INTO user(first_name, last_name, email, city, state_name, passwd) VALUES('$_POST[first_name]', '$_POST[last_name]', '$_POST[email]', '$_POST[city]', '$_POST[state_name]', '$_POST[passwd]', NOW())";
-$db->query($query);
+// Check if the user already exists
+$sql = "SELECT * FROM users WHERE email = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$email]);
+
+if ($stmt->rowCount() > 0) {
+  echo "User already exists with this email.";
+} else {
+  // Insert the new user into the database
+  $sql = "INSERT INTO users (first_name, last_name, email, city, state_name, passwd, date_added) VALUES (?, ?, ?, ?, ?, ?, NOW())";
+  $stmt = $pdo->prepare($sql);
+  $result = $stmt->execute([$first_name, $last_name, $email, $city, $state, $hashed_password]);
+
+  if ($result) {
+    echo "Registration successful. Please <a href='login.php'>Login</a>.";
+  } else {
+    echo "Registration failed. Please try again.";
+  }
+}
+?>
+
+//$query = "INSERT INTO user(first_name, last_name, email, city, state_name, passwd) VALUES('$_POST[first_name]', '$_POST[last_name]', '$_POST[email]', '$_POST[city]', '$_POST[state_name]', '$_POST[passwd]', NOW())";
+//$db->query($query);
 
 ?>
 
