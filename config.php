d50b46d04690f01683c7bd477d157715143d0b37
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-26 12:51:01 -0400

    update config

--- config.php
+++ config.php
@@ -10,7 +10,5 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$connection = new mysqli($servername, $username, $password, $dbname);
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
