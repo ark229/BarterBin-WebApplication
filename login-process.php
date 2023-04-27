56bea4531c59fcea6548018368d8b3e60456c4c1
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-27 19:40:47 -0400

    update

--- login-process.php
+++ login-process.php
@@ -16,7 +16,7 @@ $result = mysqli_stmt_get_result($stmt);
 if ($row = mysqli_fetch_assoc($result)) {
     // Compare the entered password with the stored plain password
     if ($password === $row['passwd']) {
-        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['user_id'] = $row['user_id'];
         $_SESSION['email'] = $row['email'];
         header("Location: main.php");
     } else {
