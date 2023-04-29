cf4bc55848ceeadaddca9cb4d6709dc69def8e4d
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-29 18:03:23 -0400

    fix nav

--- review.php
+++ review.php
@@ -27,18 +27,8 @@ require_once('nav2.php');
 </head>
 
 <body style="color: var(--bs-orange);">
-    <nav class="navbar navbar-light navbar-expand-md py-3" style="border-bottom: 3px solid var(--bs-gray-500);margin-bottom: 60px;">
-        <div class="container"><a class="navbar-brand d-flex align-items-center" href="index.html" style="width: 340px;height: 230px;"><img src="assets/img/barter-bin-high-resolution-logo-color-on-transparent-background.png" style="width: 330px;"></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-2">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link active" href="register.html" style="font-weight: bold;color: var(--bs-green);font-size: 20px;">Register</a></li>
-                    <li class="nav-item"><a class="nav-link" href="contact.html" style="color: var(--bs-green);font-weight: bold;font-size: 20px;">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="login.html" style="color: var(--bs-green);font-weight: bold;font-size: 20px;">Login</a></li>
+    <!-- NAV GOES HERE -->
 
-                </ul>
-            </div>
-        </div>
-    </nav>
 
     <div class="container">
         <h1 class="mt-5 mb-5">Review & Rating System in PHP & Mysql using Ajax</h1>
