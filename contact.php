c03776512be9b1c7a596aa5ac8ab548c3611c4d2
Arnetta Knight <arnetta.knight@gmail.com>
2023-04-30 11:23:37 -0400

    re-style contact section

--- contact.php
+++ contact.php
@@ -37,10 +37,10 @@ require_once('nav.php');
                             <p class="text-muted">If you have any questions, concerns, or problems registering or
                                 logging into your account please feel free to contact us. We will get back to you
                                 within 48 hours.</p>
-                            <div class="mb-3"><label class="form-label" for="fullname">Name</label><input class="form-control" type="text" id="fullname" name="fullname"></div>
-                            <div class="mb-3"><label class="form-label" for="email">Email</label><input class="form-control" type="email" id="email" name="email"></div>
-                            <div class="mb-3"><label class="form-label" for="messages">Message</label><textarea class="form-control" id="messages" name="messages" rows="6"></textarea></div>
-                            <div class="mb-3"><button class="btn btn-primary" type="submit">Send </button></div>
+                            <div class="mb-3"><label class="form-label" style="color: #212529;" for="fullname">Name</label><input class="form-control" type="text" id="fullname" name="fullname"></div>
+                            <div class="mb-3"><label class="form-label" style="color: #212529;" for="email">Email</label><input class="form-control" type="email" id="email" name="email"></div>
+                            <div class="mb-3"><label class="form-label" style="color: #212529;" for="messages">Message</label><textarea class="form-control" id="messages" name="messages" rows="6"></textarea></div>
+                            <div class="mb-3"><button class="btn btn-primary" style="background-color: #198754;" type="submit">Send </button></div>
                         </form>
                     </div>
                 </div>
@@ -53,6 +53,7 @@ require_once('nav.php');
                 <li class="list-inline-item me-4"><a class="link-secondary" href="about.php">About</a></li>
                 <li class="list-inline-item me-4"><a class="link-secondary" href="contact.php">Contact</a></li>
                 <li class="list-inline-item"><a class="link-secondary" href="register.php">Register</a></li>
+                <li class="list-inline-item"><a class="link-secondary" href="review.php">Leave Review</a></li>
             </ul>
             <ul class="list-inline">
                 <li class="list-inline-item me-4"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-facebook">
